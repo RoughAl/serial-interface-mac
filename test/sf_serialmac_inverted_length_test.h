4bb96a6bedd132196f694040679e649ed38340ff
Adrian Antonana <adrian.antonana@stackforce.de>
2018-01-22 13:48:37 +0100

    tests: subclass SerialMacTest class for inverted length field enabled/disabled
    test suites.The SerialMacInvertedLengthTest/SerialMacNoInvertedLengthTest classes
    will respectively enable/disable the inverted length field.
    
    gh-13

---
+++ test/sf_serialmac_inverted_length_test.h
@@ -0,0 +1,23 @@
+/**
+ * @code
+ *  ___ _____ _   ___ _  _____ ___  ___  ___ ___
+ * / __|_   _/_\ / __| |/ / __/ _ \| _ \/ __| __|
+ * \__ \ | |/ _ \ (__| ' <| _| (_) |   / (__| _|
+ * |___/ |_/_/ \_\___|_|\_\_| \___/|_|_\\___|___|
+ * embedded.connectivity.solutions.==============
+ * @endcode
+ *
+ * @file
+ * @copyright  STACKFORCE GmbH, Heitersheim, Germany, http://www.stackforce.de
+ * @author     Adrian Antonana <adrian.antonana@stackforce.de>
+ * @brief      Serial MAC unit tests class with enabled inverted length field
+ */
+
+#include "sf_serialmac_test.h"
+
+class SerialMacInvertedLengthTest : public SerialMacTest {
+
+    public:
+        SerialMacInvertedLengthTest();
+        virtual ~SerialMacInvertedLengthTest();
+};
---
+++ test/sf_serialmac_no_inverted_length_test.h
@@ -0,0 +1,23 @@
+/**
+ * @code
+ *  ___ _____ _   ___ _  _____ ___  ___  ___ ___
+ * / __|_   _/_\ / __| |/ / __/ _ \| _ \/ __| __|
+ * \__ \ | |/ _ \ (__| ' <| _| (_) |   / (__| _|
+ * |___/ |_/_/ \_\___|_|\_\_| \___/|_|_\\___|___|
+ * embedded.connectivity.solutions.==============
+ * @endcode
+ *
+ * @file
+ * @copyright  STACKFORCE GmbH, Heitersheim, Germany, http://www.stackforce.de
+ * @author     Adrian Antonana <adrian.antonana@stackforce.de>
+ * @brief      Serial MAC unit tests class with disabled inverted length field
+ */
+
+#include "sf_serialmac_test.h"
+
+class SerialMacNoInvertedLengthTest : public SerialMacTest {
+
+    public:
+        SerialMacNoInvertedLengthTest();
+        virtual ~SerialMacNoInvertedLengthTest();
+};
