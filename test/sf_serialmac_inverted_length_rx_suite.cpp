b9286e823b9a5746c489bdbcbec074f5d0f6648d
Adrian Antonana <adrian.antonana@stackforce.de>
2018-01-23 16:22:05 +0100

    tests/cmake: add Rx tests suites for both inverted length field enabled/disabled cases
    gh-13

---
+++ test/sf_serialmac_inverted_length_rx_suite.cpp
@@ -0,0 +1,67 @@
+/**
+ * @code
+ *  ___ _____ _   ___ _  _____ ___  ___  ___ ___
+ * / __|_   _/_\ / __| |/ / __/ _ \| _ \/ __| __|
+ * \__ \ | |/ _ \ (__| ' <| _| (_) |   / (__| _|
+ * |___/ |_/_/ \_\___|_|\_\_| \___/|_|_\\___|___|
+ * embedded.connectivity.solutions.==============
+ * @endcode
+ *
+ * @file
+ * @copyright  STACKFORCE GmbH, Heitersheim, Germany, http://www.stackforce.de
+ * @author     Adrian Antonana <adrian.antonana@stackforce.de>
+ * @brief      Inverted length Serial MAC Rx test suite.
+ */
+
+#include "sf_serialmac_inverted_length_test.h"
+#include "sf_serialmac.h"
+#include "sf_crc.h"
+
+/**
+ * @brief Test max possible frame payload edge case.
+ */
+TEST_F(SerialMacInvertedLengthTest, ReceiveMaxSizeFrame) {
+
+    sf_serialmac_return macRet;
+    std::vector<uint8_t> rxExpectedPayload;
+    uint16_t i;
+
+    for(i=0; i<UINT16_MAX; i++) {
+        rxExpectedPayload.push_back((uint8_t)i);
+    }
+
+    SetupHalBuffer(rxExpectedPayload);
+    InitSerialMac();
+
+    macRet = sf_serialmac_hal_rx_callback(serialMacCtxt);
+    EXPECT_EQ(macRet, SF_SERIALMAC_RETURN_SUCCESS)
+    << "HAL Rx callback failed";
+
+    EXPECT_EQ(rxExpectedPayload, SerialMacInvertedLengthTest::rxPayload)
+    << "Received unexpected payload";
+}
+
+/**
+ * @brief Test correct frame payloads are received for frames sizes
+ *        from 1 to MAX_TEST_PAYLOAD_LEN.
+ */
+TEST_F(SerialMacInvertedLengthTest, ReceiveFrames) {
+
+    sf_serialmac_return macRet;
+    std::vector<uint8_t> rxExpectedPayload;
+    int i;
+
+    InitSerialMac();
+
+    for(i=0; i<MAX_TEST_PAYLOAD_LEN; i++) {
+        rxExpectedPayload.push_back((uint8_t)i);
+        SetupHalBuffer(rxExpectedPayload);
+
+        macRet = sf_serialmac_hal_rx_callback(serialMacCtxt);
+        EXPECT_EQ(macRet, SF_SERIALMAC_RETURN_SUCCESS)
+        << "HAL Rx callback failed";
+
+        EXPECT_EQ(rxExpectedPayload, SerialMacInvertedLengthTest::rxPayload)
+        << "Received unexpected payload";
+    }
+}
