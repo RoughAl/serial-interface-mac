d188fad86a55e421ae91f6b6e4be76cb067ea9d4
Adrian Antonana <adrian.antonana@stackforce.de>
2018-01-24 09:02:02 +0100

    tests: add remaining NULL callback init tests
    gh-13

--- test/sf_serialmac_configurable_init_suite.cpp
+++ test/sf_serialmac_configurable_init_suite.cpp
@@ -160,3 +160,147 @@ TEST_F(SerialMacInvertedLengthTest, WrongHalWriteInit) {
     EXPECT_NE(macRet, SF_SERIALMAC_RETURN_SUCCESS)
     << "Mac init should have failed";
 }
+
+/**
+ * @brief Test attempt to init with a NULL read frame callback function.
+ */
+TEST_F(SerialMacInvertedLengthTest, WrongReadFrameInit) {
+
+    sf_serialmac_return macRet;
+
+    macRet = sf_serialmac_init (serialMacCtxt,
+                                    dummyPortHandle,
+                                    (SF_SERIALMAC_HAL_READ_FUNCTION) HalReadCb,
+                                    (SF_SERIALMAC_HAL_READ_WAIT_FUNCTION) HalReadWaitingCb,
+                                    (SF_SERIALMAC_HAL_WRITE_FUNCTION) HalWriteCb,
+                                    NULL,
+                                    (SF_SERIALMAC_EVENT) ReadBufferCb,
+                                    (SF_SERIALMAC_EVENT) ReadSyncByteCb,
+                                    (SF_SERIALMAC_EVENT) WriteFrameCb,
+                                    (SF_SERIALMAC_EVENT) WriteBufferCb,
+                                    (SF_SERIALMAC_EVENT_ERROR) MacErrorCb,
+                                    invertedLengthField);
+
+    EXPECT_NE(macRet, SF_SERIALMAC_RETURN_SUCCESS)
+    << "Mac init should have failed";
+}
+
+/**
+ * @brief Test attempt to init with a NULL read buffer callback function.
+ */
+TEST_F(SerialMacInvertedLengthTest, WrongReadBufferInit) {
+
+    sf_serialmac_return macRet;
+
+    macRet = sf_serialmac_init (serialMacCtxt,
+                                    dummyPortHandle,
+                                    (SF_SERIALMAC_HAL_READ_FUNCTION) HalReadCb,
+                                    (SF_SERIALMAC_HAL_READ_WAIT_FUNCTION) HalReadWaitingCb,
+                                    (SF_SERIALMAC_HAL_WRITE_FUNCTION) HalWriteCb,
+                                    (SF_SERIALMAC_EVENT) ReadFrameCb,
+                                    NULL,
+                                    (SF_SERIALMAC_EVENT) ReadSyncByteCb,
+                                    (SF_SERIALMAC_EVENT) WriteFrameCb,
+                                    (SF_SERIALMAC_EVENT) WriteBufferCb,
+                                    (SF_SERIALMAC_EVENT_ERROR) MacErrorCb,
+                                    invertedLengthField);
+
+    EXPECT_NE(macRet, SF_SERIALMAC_RETURN_SUCCESS)
+    << "Mac init should have failed";
+}
+
+/**
+ * @brief Test attempt to init with a NULL sync byte callback function.
+ */
+TEST_F(SerialMacInvertedLengthTest, WrongReadSyncByteInit) {
+
+    sf_serialmac_return macRet;
+
+    macRet = sf_serialmac_init (serialMacCtxt,
+                                dummyPortHandle,
+                                (SF_SERIALMAC_HAL_READ_FUNCTION) HalReadCb,
+                                (SF_SERIALMAC_HAL_READ_WAIT_FUNCTION) HalReadWaitingCb,
+                                (SF_SERIALMAC_HAL_WRITE_FUNCTION) HalWriteCb,
+                                (SF_SERIALMAC_EVENT) ReadFrameCb,
+                                (SF_SERIALMAC_EVENT) ReadBufferCb,
+                                NULL,
+                                (SF_SERIALMAC_EVENT) WriteFrameCb,
+                                (SF_SERIALMAC_EVENT) WriteBufferCb,
+                                (SF_SERIALMAC_EVENT_ERROR) MacErrorCb,
+                                invertedLengthField);
+
+    EXPECT_NE(macRet, SF_SERIALMAC_RETURN_SUCCESS)
+    << "Mac init should have failed";
+}
+
+/**
+ * @brief Test attempt to init with a NULL write frame callback function.
+ */
+TEST_F(SerialMacInvertedLengthTest, WrongWriteFrameInit) {
+
+    sf_serialmac_return macRet;
+
+    macRet = sf_serialmac_init (serialMacCtxt,
+                                dummyPortHandle,
+                                (SF_SERIALMAC_HAL_READ_FUNCTION) HalReadCb,
+                                (SF_SERIALMAC_HAL_READ_WAIT_FUNCTION) HalReadWaitingCb,
+                                (SF_SERIALMAC_HAL_WRITE_FUNCTION) HalWriteCb,
+                                (SF_SERIALMAC_EVENT) ReadFrameCb,
+                                (SF_SERIALMAC_EVENT) ReadBufferCb,
+                                (SF_SERIALMAC_EVENT) ReadSyncByteCb,
+                                NULL,
+                                (SF_SERIALMAC_EVENT) WriteBufferCb,
+                                (SF_SERIALMAC_EVENT_ERROR) MacErrorCb,
+                                invertedLengthField);
+
+    EXPECT_NE(macRet, SF_SERIALMAC_RETURN_SUCCESS)
+    << "Mac init should have failed";
+}
+
+/**
+ * @brief Test attempt to init with a NULL write buffer callback function.
+ */
+TEST_F(SerialMacInvertedLengthTest, WrongWriteBufferInit) {
+
+    sf_serialmac_return macRet;
+
+    macRet = sf_serialmac_init (serialMacCtxt,
+                                dummyPortHandle,
+                                (SF_SERIALMAC_HAL_READ_FUNCTION) HalReadCb,
+                                (SF_SERIALMAC_HAL_READ_WAIT_FUNCTION) HalReadWaitingCb,
+                                (SF_SERIALMAC_HAL_WRITE_FUNCTION) HalWriteCb,
+                                (SF_SERIALMAC_EVENT) ReadFrameCb,
+                                (SF_SERIALMAC_EVENT) ReadBufferCb,
+                                (SF_SERIALMAC_EVENT) ReadSyncByteCb,
+                                (SF_SERIALMAC_EVENT) WriteFrameCb,
+                                NULL,
+                                (SF_SERIALMAC_EVENT_ERROR) MacErrorCb,
+                                invertedLengthField);
+
+    EXPECT_NE(macRet, SF_SERIALMAC_RETURN_SUCCESS)
+    << "Mac init should have failed";
+}
+
+/**
+ * @brief Test attempt to init with a NULL write buffer callback function.
+ */
+TEST_F(SerialMacInvertedLengthTest, WrongErrorInit) {
+
+    sf_serialmac_return macRet;
+
+    macRet = sf_serialmac_init (serialMacCtxt,
+                                dummyPortHandle,
+                                (SF_SERIALMAC_HAL_READ_FUNCTION) HalReadCb,
+                                (SF_SERIALMAC_HAL_READ_WAIT_FUNCTION) HalReadWaitingCb,
+                                (SF_SERIALMAC_HAL_WRITE_FUNCTION) HalWriteCb,
+                                (SF_SERIALMAC_EVENT) ReadFrameCb,
+                                (SF_SERIALMAC_EVENT) ReadBufferCb,
+                                (SF_SERIALMAC_EVENT) ReadSyncByteCb,
+                                (SF_SERIALMAC_EVENT) WriteFrameCb,
+                                (SF_SERIALMAC_EVENT) WriteBufferCb,
+                                NULL,
+                                invertedLengthField);
+
+    EXPECT_NE(macRet, SF_SERIALMAC_RETURN_SUCCESS)
+    << "Mac init should have failed";
+}
